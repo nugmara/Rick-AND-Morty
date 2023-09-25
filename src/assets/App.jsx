348bf5139ca35d07e77accb440950821de0b1fa5
Dagmara Grabowska <dagmarag0@gmail.com>
2023-09-25 21:08:05 +0200

    Tailwind setup done

---
+++ src/assets/App.jsx
@@ -0,0 +1,17 @@
+
+export default function App() {
+    return (
+      
+      <header className="py-8 px-4 mx-auto max-w-xl lg:py-16 lg:px-6">
+        <div className="mx-auto text-center mb-8 lg:mb-16">
+            <h2 className="mb-4 text-5xl tracking-tight font-extrabold text-white">
+                Welcome To the Rick and Morty website!
+            </h2>
+            <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">
+                Here you will have access to about hundreds of characters, images, locations and episodes of your favorite show 🥒 
+            </p>
+        </div>
+      </header>
+    
+    )
+  }
