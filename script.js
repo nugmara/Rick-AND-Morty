30cbd35d3102b9f805859593cf104ffc8a55f500
Dagmara Grabowska <dagmarag0@gmail.com>
2023-09-26 19:04:09 +0200

    Display name and images on the screen with dom

---
+++ script.js
@@ -0,0 +1,49 @@
+const baseURL = "https://rickandmortyapi.com/api/character";
+let allCharacters = []
+
+const fetchAllCharacters = async (url) => {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+
+        if (response.ok) {
+            allCharacters = allCharacters.concat(data.results)
+            console.log("All characters have been fectched:", allCharacters);
+
+            if (data.info.next) {
+                await fetchAllCharacters(data.info.next)
+            } else {
+                renderCharacters()
+            }
+        } else {
+            console.log("Error fetching characters:", data.error )
+        }
+    } catch (error) {
+        console.log("An error occurred", error)
+    }
+}
+
+function renderCharacters() {
+    const charactersList = document.getElementById("characters-list");
+    allCharacters.forEach((character) => {
+        const listItem = document.createElement("li");
+        const characterName = document.createElement("span");
+        const characterImage = document.createElement("img");
+
+        // Set character name
+        characterName.textContent = character.name
+
+        // Set character image source
+        characterImage.src = character.image
+
+        // Append name and image to the list item
+        listItem.appendChild(characterName)
+        listItem.appendChild(characterImage)
+
+        // Append the list item to the characters list
+        charactersList.appendChild(listItem)
+
+    })
+}
+
+fetchAllCharacters(baseURL)
